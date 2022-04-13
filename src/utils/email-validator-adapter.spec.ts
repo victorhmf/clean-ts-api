109311017b25ee0edbc6b7788dbb68bde1633cec
Victor Fernandes <victor.hmfd@gmail.com>
2022-04-12 21:39:32 -0300

    test: ensure EmailValidatorAdpater calls validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -12,12 +12,23 @@ describe('EmailValidatorAdapter', () => {
     const sut = new EmailValidatorAdapter()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
+
     expect(isValid).toBe(false)
   })
 
   test('should return true if validator returns true ', () => {
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('invalid_email@mail.com')
+
     expect(isValid).toBe(true)
+  })
+
+  test('should call validator with correct email ', () => {
+    const sut = new EmailValidatorAdapter()
+    const email = 'any_email@mail.com'
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid(email)
+
+    expect(isEmailSpy).toHaveBeenLastCalledWith(email)
   })
 })
